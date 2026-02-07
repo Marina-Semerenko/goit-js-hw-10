a651870ffc4494e5f64b3794054e1d0746e2c2ce
Marina-Semerenko <marina.miram@gmail.com>
2026-02-07 11:45:14 +0000

    Deploying to gh-pages from @ Marina-Semerenko/goit-js-hw-10@3899253c36b9742821471652020383cf57eabf76 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */document.querySelector("button[data-start]");document.querySelector("button[data-action-stop]");document.querySelector(".clockface");
+//# sourceMappingURL=1-timer.js.map
